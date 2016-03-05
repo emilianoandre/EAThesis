6021ccdf2a988cae2e8e73c017934e64830d8df0
Emiliano Andre <emilianoandre@gmail.com>
2016-03-05 11:46:20 -0300

    VOYAG-2 // Creating Controllers Project

---
+++ Code/VoyagerController/src/main/java/Voyager/Controller/App.java
@@ -0,0 +1,13 @@
+package Voyager.Controller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
